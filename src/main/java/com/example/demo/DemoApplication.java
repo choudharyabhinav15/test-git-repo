c43e7da9e149816e2ae0892e371614f98e9de16a
Abhinav Choudhary <abhinav.choudhary@softtek.com>
2021-10-05 12:20:52 +0530

    feature 2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("TEST");
+		System.out.println("TEST - NOT TO BE INCLUDED");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
